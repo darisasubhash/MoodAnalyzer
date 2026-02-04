57e5a083a966b351677c7704071bd698e18bbed8
darisasubhash <subbusubbu8193@gmail.com>
2026-02-04 16:09:57 +0530

    [Subhash] : Using enum to differntiate mood analysis

---
+++ src/main/java/com/moodanalyser/MoodAnalysisException.java
@@ -0,0 +1,15 @@
+package com.moodanalyser;
+
+public class MoodAnalysisException extends Exception{
+    public enum ExceptionType {
+        NULL,
+        EMPTY
+    }
+
+    public ExceptionType type;
+
+    public MoodAnalysisException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+}
