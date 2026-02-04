1eea950d1b4e5c791252d9d9f473dbccc29fb565
darisasubhash <subbusubbu8193@gmail.com>
2026-02-04 15:47:06 +0530

    [Subhash] : Handle exception if user provide invalid

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -15,10 +15,4 @@ public class MoodAnalyserTest {
         MoodAnalyser analyser=new MoodAnalyser("I am in Happy Mood");
         assertEquals("HAPPY",analyser.analyseMood());
     }
-    @Test
-    public void givenMessageNullReturnsHappy(){
-        MoodAnalyser analyser=new MoodAnalyser(null);
-        assertEquals("HAPPY",analyser.analyseMood());
-    }
-
 }
